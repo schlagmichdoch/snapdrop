5fdd28cccef2a89b6d0d46941a63faaed0340fc1
schlagmichdoch <schlagmichdoch@users.noreply.github.com>
2023-01-07 02:12:44 +0100

    comment in stun server again

--- public/scripts/network.js
+++ public/scripts/network.js
@@ -572,9 +572,9 @@ class Events {
 RTCPeer.config = {
     'sdpSemantics': 'unified-plan',
     iceServers: [
-        // {
-        //     urls: 'stun:stun.l.google.com:19302'
-        // },
+        {
+            urls: 'stun:stun.l.google.com:19302'
+        },
         {
             urls: "turn:openrelay.metered.ca:443",
             username: "openrelayproject",
